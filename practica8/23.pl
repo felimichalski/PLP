2a573a21c420f6ddc72b1a46df86316236e1fdc9
Felipe Michalski <fmichalski@veritran.com>
2025-06-29 02:58:52 -0300

    ej23

--- practica8/23.pl
+++ practica8/23.pl
@@ -1,16 +1,37 @@
 desde(X, X).
-desde(X, Y):-
-    X1 is X + 1,
-    desde(X1, Y).
+desde(X, Y):- X1 is X + 1, desde(X1, Y).
 
 % arbol(-A)
-arbol(A):- desde(0, Y), arbolPorNivel(Y, A).
+arbol(A):- desde(0, N), arbolDeAltura(N, A).
 
-arbolPorNivel(0, nil).
-arbolPorNivel(N, bin(AI, _, AD)):-
+%arbolDeAltura(+A, -T)
+arbolDeAltura(0, nil).
+arbolDeAltura(N, bin(I, _, D)):-
     N > 0,
     N1 is N - 1,
-    between(0, N1, NI),
-    ND is N1 - NI,
-    arbolPorNivel(NI, AI),
-    arbolPorNivel(ND, AD).
+    between(0, N1, N2),
+    between(0, N1, N3),
+    not((
+        N2 < N1,
+        N3 < N1
+    )),
+    arbolDeAltura(N2, I),
+    arbolDeAltura(N3, D).
+
+% nodosEn(?A, +L)
+nodosEn(nil, _).
+nodosEn(bin(I, R, D), L):- member(R, L), nodosEn(I, L), nodosEn(D, L).
+
+% sinRepEn(-A, +L)
+sinRepEn(nil, _).
+sinRepEn(bin(I, R, D), XS):-
+    member(R, XS),
+    select(R, XS, YS),
+    sinRepEn(I, YS),
+    sinRepEn(D, YS),
+    listaDeNodos(I, NI),
+    listaDeNodos(D, ND),
+    not((member(N, NI), member(N, ND))).
+
+listaDeNodos(nil, []).
+listaDeNodos(bin(I, R, D), [R|NS]):- listaDeNodos(I, NI), listaDeNodos(D, NR), append(NI, NR, NS).
